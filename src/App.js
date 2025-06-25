0647c2a8d8fc605c2d51845838b02914578593a4
buildwithprakash <korwanp@gmail.com>
2025-06-25 23:32:32 +0530

    appjs has updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Prakash</p>
+        <p>Hello Prakash How are you</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
